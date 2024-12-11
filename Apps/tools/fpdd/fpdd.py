47f90881f82341144565ac4033d55bf12e43cdbd
MrDexstor-Term <mrdexstor@dexpr.ru>
2024-12-11 22:20:17 +0300

    Чето сделано

--- Apps/tools/fpdd/fpdd.py
+++ Apps/tools/fpdd/fpdd.py
@@ -1,6 +1,6 @@
 from Lib.UI import render, Page
 from ServerAPI import write_offs
-
+from assets.models import FPDD_DocConnectED
 
 def main(request):
     doc = write_offs.getList(request)
@@ -10,11 +10,38 @@ def main(request):
 
 
 def docPanel(request, id):
+    context = {}
     doc = write_offs.offsData(request, id)
+    context['off'] = doc
     write_offs_positions = write_offs.positions(request, doc["id"])
-    page = Page('FDPP | WebGK', 'Списание товара доставки', f'{doc["docNumber"]}')
+    context['positions'] = write_offs_positions
+    if FPDD_DocConnectED.objects.filter(writeOffs=id).count() == 1:
+        # Найдена запись
+        dmp = FPDD_DocConnectED.objects.get(writeOffs=id)
+        
+        if dmp.liCreationState == 'none':
+            context['eddc_e'] = 'Документ коррекции отсувствует в BO или его Id обвязки некорректен!'
+        elif dmp.liCreationState == 'started':
+            context['eddc_e'] = 'Документ коррекции создается в BO, ожидайте завершиния процесса'
+        elif dmp.liCreationState == 'created':
+            context['eddc_i'] = dmp
+        elif dmp.liCreationState == 'completed':
+            context['eddc_i'] = dmp
+        else:
+            pass
+            
+        context['li_positions'] = {}
+        
+    elif FPDD_DocConnectED.objects.filter(writeOffs=id).count() == 0:
+        # Записи нет
+        pass
+    else:
+        # Какая то хуета, более одной записи или что то ещё
+        pass
+    
+    page = Page('ФПДЭД | WebGK', 'Списание товара доставки', f'{doc["docNumber"]}')
     page.returnUrl('/GK/tools/fpdd/')
-    return render(request, page, 'tools/fpdd/doc_panel.html', {"off": doc, "positions": write_offs_positions})
+    return render(request, page, 'tools/fpdd/doc_panel.html', context)
     
  
 def createLocal(request, id):
