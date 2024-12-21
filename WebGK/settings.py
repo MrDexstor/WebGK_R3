c6c17684a17ae1293cdf546649d4204a07958ac2
MrDexstor-Term <mrdexstor@dexpr.ru>
2024-12-21 19:21:09 +0300

    Добавлены файлы celery

--- WebGK/settings.py
+++ WebGK/settings.py
@@ -116,3 +116,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'Core.User'
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
