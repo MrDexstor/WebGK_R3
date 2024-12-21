337918d27fc786b6a95e81f14ab49fd28d4533c6
MrDexstor-Term <mrdexstor@dexpr.ru>
2024-12-21 21:13:05 +0300

    fix

--- script/profile.sh
+++ script/profile.sh
@@ -15,3 +15,10 @@ echo 'Конфигурирование завершено!'
 echo 'Формирую файлы для запуска систем...'
 cd 
 cp ~/WebGK_R3/script/rsgk.sh /data/data/com.termux/files/usr/bin/rsgk
+cd 
+cd WebGK_R3 
+python manage.py makemigrations
+python manage.py migrate
+echo 'Регистрация IT Соотрудника'
+python manage.py createsuperuser
+rsgk run
