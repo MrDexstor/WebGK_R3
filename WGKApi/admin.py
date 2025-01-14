34b9c77d8128ecb23e2d89b1407ad265bf0dfd26
MrDexstor-Term <mrdexstor@dexpr.ru>
2025-01-15 00:09:30 +0300

    Добавлена новая таблица для локального хранения товарных груп, добавлено отображение в админке и созданы файлы миграции для этой таблицы

--- WGKApi/admin.py
+++ WGKApi/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from WGKApi.models import Files
+from WGKApi.models import Files, ProductGroup
 # Register your models here.
 admin.site.register(Files)
+admin.site.register(ProductGroup)
