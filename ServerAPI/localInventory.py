97772fda1265965cc13dee09492445241a025e98
MrDexstor-Term <mrdexstor@dexpr.ru>
2025-01-15 23:46:49 +0300

    ll

--- ServerAPI/localInventory.py
+++ ServerAPI/localInventory.py
@@ -1,13 +1,13 @@
-import request
+import requests
 from Lib.BO_Checks import check_network, sessionCheck
 from Core.config import BO_Url
 from Lib import BOm
-from datetime import datemine
+from datetime import datetime
 
 
 @check_network('li-create')
 @sessionCheck
-def createDoc(request, name, positions = []):
+def createDoc(request, name, positions =[]):
     url = f'{BO_Url()}/api/local-inventories'
     current_datetime = datetime.now()
     formatted_datetime = current_datetime.strftime("%d.%m.%Y %H:%M:%S")
